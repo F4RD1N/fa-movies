77dcc8b809d856426198367829bbf36d0aaeb70d
F4RD1N <fardin5727@gmail.com>
2022-05-23 01:32:47 +0430

    fix some bugs

--- src/components/MoviePage/Header.js
+++ src/components/MoviePage/Header.js
@@ -25,21 +25,16 @@ const Header = ({data}) => {
      <div className="absolute bottom-0 w-full h-1/2 sliderGradient"></div>
           
           
-      <div className="absolute bottom-0 left-0 right-0 px-6 flex flex-col">
-          <h2 className="text-white text-2xl font-semibold break-all">{title ? title : name }</h2>
-          <h3 className="mt-2 text-base truncate w-full">{tagline && tagline.replaceAll('.', '')}</h3>
-      <ul className="mt-6 space-x-2 text-textSecondary">
-        <li className="badge">{original_language && getFullLanguageName(original_language)}</li>
-        <li className="badge">{release_date ? sliceYearFromDate(release_date) : first_air_date && sliceYearFromDate(first_air_date)}</li>
-        <li className="badge text-yellow-400">{vote_average ? vote_average : <span className="text-sm">Coming Soon</span>}</li>
-        <li className="badge">{genres && genres.length && genres[0].name}</li>
+      <div className="absolute bottom-0 left-0 right-0 px-6 -translate-y-4 flex flex-col justify-center items-center">
+          <h2 className="text-white text-xl font-semibold break-all">{title ? title : name }</h2>
+          <h3 className="mt-2 text-sm truncate w-full text-center">{tagline && tagline.replaceAll('.', '')}</h3>
+      <ul className="mt-4 space-x-4 text-white flex items-center">
+        <li className="badge text-sm">{original_language && getFullLanguageName(original_language)}</li>
+        <li className="badge text-sm">{release_date ? sliceYearFromDate(release_date) : first_air_date && sliceYearFromDate(first_air_date)}</li>
+        <li className="badge text-3xl text-yellow-500">{vote_average ? vote_average : <span className="text-sm">Not Released</span>}</li>
+        <li className="badge text-sm">{genres && genres.length && genres[0].name}</li>
+        <li className="badge text-sm">+{genres && genres.length && getAgeLimit(genres[0].name)}</li>
        </ul>
-       
-       <div className="absolute right-0 bottom-8 mr-5 flex flex-col justify-center items-center space-y-2">
-       <div className="w-16 h-16 flex justify-center items-center rounded-full bg-primary text-white font-bold active:saturate-150"><i className="ri-download-line text-2xl"></i></div>
-        <span className="text-xs">02:39:52</span>
-        </div>
-
     </div>
     
     
