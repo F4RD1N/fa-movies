ebb084915d7d80ab4323131ff26c9587ddd529f3
F4RD1N <fardin5727@gmail.com>
2022-07-06 00:56:53 +0430

    complete use country detection

--- src/services/userCountryService.js
+++ src/services/userCountryService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const userCountryService = async () => {
-  const response = await axios.get("https://api.db-ip.com/v2/free/self");
+  const response = await axios.get(`https://api.db-ip.com/v2/free/self?timestamp=${new Date().getTime()}`);
   return response;
 };
 
