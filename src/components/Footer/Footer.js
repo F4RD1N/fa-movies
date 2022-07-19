8b818a05fdef2a44b3c4c30044e37add7df6b3f1
F4RD1N <fardin5727@gmail.com>
2022-07-19 22:40:32 +0430

    change footer msg

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 const Footer = () => {
   const githubLink = 'https://github.com/F4RD1N'
   return (
-    <div className='bg-secondary text-white py-5 flex justify-center items-center mt-16'>Made wtih ❤️ by <a className='ml-2 text-blue-400' href={githubLink} target='_blank'>#F4RD1N</a></div>
+    <div className='bg-secondary text-white py-5 flex justify-center items-center mt-16'>Made width ❤️ by <a className='ml-2 text-blue-400' href={githubLink} target='_blank'>#F4RD1N</a></div>
   )
 }
 
